b58c19abf697f81af8d247fab5036a3b56c6447f
bromitekrfilter <78836116+bromitekrfilter@users.noreply.github.com>
2021-02-10 14:50:41 +0900

    Add files

---
+++ download.sh
@@ -0,0 +1,12 @@
+
+wget https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt
+wget https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt
+wget https://easylist.to/easylist/easylist.txt
+wget https://easylist.to/easylist/easyprivacy.txt
+wget https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt
+wget https://raw.githubusercontent.com/brave/adblock-lists/master/brave-unbreak.txt
+wget https://raw.githubusercontent.com/brave/adblock-lists/master/coin-miners.txt
+wget https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt
+wget https://raw.githubusercontent.com/List-KR/List-KR/master/filter.txt
+wget https://raw.githubusercontent.com/yous/YousList/master/youslist.txt
+cp adblockplus.txt.bak adblockplus.txt
---
+++ make.sh
@@ -0,0 +1,4 @@
+ruleset_converter --input_format=filter-list \
+		--output_format=unindexed-ruleset \
+                --input_files=easyprivacy.txt,easylist.txt,youslist.txt,filter.txt,filters.txt,resource-abuse.txt,adblockplus.txt,unbreak.txt,brave-unbreak.txt,coin-miners.txt,abp-filters-anti-cv.txt \
+		--output_file=filters.dat
